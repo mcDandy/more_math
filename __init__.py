f04c2320ed3323386edd277bee421a10e5a1ece7
mcDandy <danda.martinek@gmail.com>
2025-12-11 21:35:57 +0100

    Moved more_math out of src, apparently it is done like that.

--- __init__.py
+++ __init__.py
@@ -1,14 +1,2 @@
-"""Top-level package for more_math."""
-
-__all__ = [
-    "NODE_CLASS_MAPPINGS",
-    "NODE_id_MAPPINGS",
-
-]
-
-__author__ = """Daniel Martinek"""
-__email__ = "danda.martinek@gmail.com"
-__version__ = "0.0.1"
-
-
+from .more_math.nodes import comfy_entrypoint
 

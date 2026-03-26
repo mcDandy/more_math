5c63ad12e1c470f837904d931095d5c656cb5c2c
Daniel Martinek <danda.martinek@gmail.com>
2026-03-26 14:14:33 +0100

    better error when something is completly broken

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
+import traceback
+
 try:
     from .more_math.nodes import comfy_entrypoint as comfy_entrypoint
 except ImportError:
-    # During testing, comfy_api may not be available
-    # This is fine - tests import modules directly
     print("Something is seriously wrong")
+    traceback.print_exc()
 
 WEB_DIRECTORY = "./web"
 __all__ = ["comfy_entrypoint", "WEB_DIRECTORY"]
